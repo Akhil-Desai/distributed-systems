e1f6d26fde3c2c3354b134a0182b8a029927083d
Akhil-prog-design <akhilrdesai@gmail.com>
2024-12-23 21:34:36 -0500

    Create file upload endpoint

--- FileStorageSystem/server/server.go
+++ FileStorageSystem/server/server.go
@@ -3,10 +3,38 @@ package main
 import (
 	"fmt"
 	"net/http"
+    "io"
+    "os"
 )
 
 func uploadFile(w http.ResponseWriter, r *http.Request){
-	return
+    fmt.Println("File upload endpoint hit!")
+
+    err := r.ParseMultipartForm(10 << 20)
+    if err != nil {
+        fmt.Println("Error parsing file", err)
+        return
+    }
+
+    file,handler,err := r.FormFile("clientFile")
+    if err != nil{
+        fmt.Println("Error", err)
+        return
+    }
+    defer file.Close()
+
+    dst,err := os.Create(handler.Filename)
+    if err != nil{
+        fmt.Println("Create file err:", err)
+    }
+
+    defer dst.Close()
+    _,err = io.Copy(dst, file)
+    if err != nil {
+        fmt.Println("Copy err:", err)
+        return
+    }
+    fmt.Fprintf(w, "File saved: %s", handler.Filename)
 }
 
 func downloadFile(w http.ResponseWriter, r *http.Request){
