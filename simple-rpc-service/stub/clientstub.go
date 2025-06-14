f4d327ad60cbdc4efbf6ba31ea30f36e15724c0f
Akhil-prog-design <akhilrdesai@gmail.com>
2025-06-14 15:05:02 -0400

    binary serialization/changed to a centralized invoke method for elementary math operations

--- simple-rpc-service/stub/clientstub.go
+++ simple-rpc-service/stub/clientstub.go
@@ -3,23 +3,22 @@ package stub
 import (
 	"fmt"
 	"net"
-	"encoding/json"
+	"encoding/binary"
 
 )
-
 
 const (
 	HOST = "localhost"
 	PORT = "5001"
 )
 
-//For poc we will execute elementary math functions
 type ClientStubInterace interface {
-	init() *net.Conn
-	add()
-	subtract()
-	multiply()
-	divide()
+	init(host string, port string) error
+	// add()
+	// subtract()
+	// multiply()
+	// divide()
+	factory(method string, a int64, b int64)
 }
 
 type RPCClientStub struct {
@@ -39,19 +38,55 @@ func (c *RPCClientStub) init(host string, port string) error{
 	return nil
 }
 
-func (c *RPCClientStub) add(a int, b int) {
+func (c *RPCClientStub) factory(method string, a int64, b int64){
 
-	request := make(map[string]interface{})
-	request["method"] = "add"
-	request["args"] = []int{a, b}
+	//cache check
+	
 
-	//We have to serialize the request
-	serializedData,err := json.Marshal(request)
+	//[length][string bytes][int64][int64]
+	buff := make([]byte, 20 + uint32(len(method)))
+	offset := 0
+
+	binary.BigEndian.PutUint32(buff[:4], uint32(len(method)))
+	offset += 4
+	copy(buff[offset:offset + len(method)], []byte(method))
+	offset += len(method)
+	binary.PutVarint(buff[offset: offset + 8], a)
+	offset += 8
+	binary.PutVarint(buff[offset: offset + 8], b)
+	offset += 8
+
+	n,err := c.conn.Write(buff)
+
 	if err != nil {
-		fmt.Println("Error serializing", err)
-		//Gracefull shutdown
+		//graceful shutdown
+		return
+	}
+	if n != offset{
+		//retry n times in the case of a network problems
+		return
 	}
 
-	c.conn.Write(serializedData)
-
 }
+
+// func (c *RPCClientStub) add(a int, b int) {
+
+// 	//[length][string bytes][int32][int32]
+// 	buff := make([]byte, 12 + uint32(len("add")))
+
+// 	offset := 0
+// 	binary.BigEndian.PutUint32(buff[:4],uint32(len("add")))
+// 	offset += 4
+// 	copy(buff[offset: offset + len("add")], []byte("add"))
+// 	offset += len("add")
+// 	binary.BigEndian.PutUint32(buff[offset: offset + 4], uint32(a))
+// 	offset += 4
+// 	binary.BigEndian.PutUint32(buff[offset: offset + 4], uint32(b))
+// 	offset += 4
+
+// 	if _, err := c.conn.Write(buff); err != nil{
+// 		//graceful shutdown
+// 		return
+// 	}
+
+// }
