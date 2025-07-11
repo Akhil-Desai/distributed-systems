def5f572656468dc1cfe628004554ff19212b33b
Akhil-prog-design <akhilrdesai@gmail.com>
2025-07-11 16:38:58 -0400

    feat: Skeleton on server

--- simple-rpc-service/stub/serverstub.go
+++ simple-rpc-service/stub/serverstub.go
@@ -3,49 +3,87 @@ package stub
 import (
 	"fmt"
 	"net"
+	"reflect"
 	"sync"
 	"strconv"
+
 )
 
 //RPCServer Stub is an Object which we want to have a cache that maintains state within that object so that cache should be intialized within the Init function. We want to pass a pointer because we want to modify the original cache to store cache keys
 
+//This is going to define our interface
+type Skeleton interface {
+	Add(a,b int64) (int64,error)
+	Subtract(a,b int64) (int64, error)
+	Multiply(a, b int64) (int64,error)
+	Divide(a, b int64) (int64, error)
+}
+
 type ServerStubber interface {
 	Init(host string, port string)
+	Register (service Skeleton)
 	handleRPCInvoke() error
 }
 
 type RPCServerStub struct {
 	ln    net.Listener
 	cache *map[string]int64
+	service Skeleton
 	sync.Mutex
 }
 
-func (s *RPCServerStub) Init(port string) error {
-	//TODO: Understand why its bad to specify a host here, its saying if you specify a host it will create at most one listener for the host ip address meaning you can't create multiple listeners on that host ip. Im assuming if you don't sepcify you can create multiple confused as to why though?
+func (s *RPCServerStub) Init(port string, service Skeleton) error {
 	ln, err := net.Listen("tcp", port)
 	if err != nil {
-		return fmt.Errorf("Error trying to listen on %s 💥", port)
+		return fmt.Errorf("error trying to listen on %s 💥", port)
 	}
 	s.ln = ln
 	serverCache := make(map[string]int64)
+	s.service = service
 	s.cache = &serverCache
 
 	return nil
 }
 
+
 func (s *RPCServerStub) handleRPCInvoke() error {
 
 	//stubbing right now
-	method := "stub"
+	methodName := "stub"
 	a := int64(1)
 	b := int64(1)
 
 	s.Mutex.Lock()
-	cacheKey := method + strconv.FormatInt(a, 10) + strconv.FormatInt(b, 10)
+	cacheKey := methodName+ strconv.FormatInt(a, 10) + strconv.FormatInt(b, 10)
 	if ret,ok := (*s.cache)[cacheKey]; ok {
 		fmt.Print(ret)
 		return nil
 	}
 	s.Mutex.Unlock()
+
+	if s.service == nil {
+		return fmt.Errorf("methods are not registerd please establish...💥")
+	}
+
+	method := reflect.ValueOf(s.service).MethodByName(methodName)
+
+	if !method.IsValid() {
+		return fmt.Errorf("invalid method invoked...💥")
+	}
+
+	inArgs := make([]reflect.Value, 2)
+	inArgs[0] = reflect.ValueOf(a)
+	inArgs[1] = reflect.ValueOf(b)
+
+	returned := method.Call(inArgs)
+
+	result := returned[0].Interface().(int64)
+	err := returned[1].Interface().(error)
+	if err != nil {
+		return fmt.Errorf("issue occured invoking %s...💥", method)
+	}
+
+	fmt.Print(result)
+
 	return nil
 }
